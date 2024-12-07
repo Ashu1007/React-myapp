d49b36754dca54cc65315cf43d08ce2d81684f32
Ashutosh Tripathi <tripathiashutosh665@gmail.com>
2024-12-07 23:53:39 +0530

    commit

--- src/task1/FlipkartNav.jsx
+++ src/task1/FlipkartNav.jsx
@@ -1,18 +1,23 @@
 import style from "./flipkartNav.module.css"
-//import { IoMdMore } from "./react-icons"
+import { IoMdMore } from "react-icons/io";
 import logo from "./Images/FlipkartImage.jpg"
+import { FaRegCircleUser } from "react-icons/fa6";
+import { BsCart3 } from "react-icons/bs";
+import { CiShop } from "react-icons/ci";
 const FlipkartNav = () => {
   return (
     <nav>
         <aside className={style.logo}>
             <img src={logo} alt="logoimg" />
          </aside>
-        <section className={style.searchContainer}> searchContainer</section>
+        <section className={style.searchContainer}> searchContainer
+
+        </section>
         <ul>
-            <li>logo</li>
-            <li>cart</li>
-            <li>become a seller</li>
-            {/* <li><IoMdMore/></li> */}
+            <li><FaRegCircleUser/>Login</li>
+            <li><BsCart3 />Cart</li>
+            <li><CiShop />Become a seller</li>
+            <li><IoMdMore/></li>
         </ul>
     </nav>
   );
